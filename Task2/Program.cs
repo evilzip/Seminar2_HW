f3795803a613eb63349b511d306c0c34d782a8fa
Anton Abdrakhmanov <ode15s@yandex.ru>
2023-02-17 01:24:00 +0300

    add validatation user value in Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -14,26 +14,33 @@ int Prompt(string message)
     return Convert.ToInt32(Console.ReadLine());      
 }
 
+bool Validator(int userValue)
+{
+    if (userValue >99)
+    {
+        return true;
+    } 
+    System.Console.WriteLine("no 3rd digit in your value ");
+    return false;
+}
+
 int digitsAmount(int userValue)
 {
     int digitCounter = 0;
     while (userValue > 0)
-    {
+        {
         digitCounter ++;
         userValue /= 10;
-    }
-    return digitCounter;
+        }
+    return digitCounter;    
 }
 
 userValue = Prompt("Enter your value");
 N = digitsAmount(userValue);
 
-if  (N >= 3)
+if  (Validator(userValue))
 {
     thirdDigit = (userValue/Convert.ToInt32(Math.Pow(10,(N-3))))%10;
     System.Console.WriteLine($"3rd digit in your value is {thirdDigit}");       
 }
-else
-{
-    System.Console.WriteLine("no 3rd number in your value ");
-}
+
