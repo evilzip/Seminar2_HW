140bf562ee0420fbb16fec5d1a2a602eb3ac173d
Anton Abdrakhmanov <ode15s@yandex.ru>
2023-02-17 02:11:35 +0300

    Task3 is done

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+int value;
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());      
+}
+
+bool validator(int userValue)
+{
+    if(userValue>0 && userValue < 8)
+    {
+        return true;
+    }
+    System.Console.WriteLine("You entered incorrect day number");
+    return false; 
+}
+
+bool weekend(int userValue)
+{
+    if (userValue == 6 || userValue == 7)
+    {
+        System.Console.WriteLine("This is weekend");
+        return true;
+    }
+    System.Console.WriteLine("This is not a weekend");
+    return false;
+}
+
+value = Prompt("Enter number of a day");
+
+if (validator(value)) 
+{
+    weekend(value);
+}
